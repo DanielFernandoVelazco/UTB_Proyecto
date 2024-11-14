518c4d3082b76d4c54f638933baa65c8c6063db6
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-11-14 11:45:20 -0500

    Se modifican las entidades de la base de datos postgres

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,29 @@
-export class Product {}
+import { User } from 'src/users/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string = uuid();
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    stock: number;
+
+    @Column({
+        nullable: true,
+    })
+    imgUrl: string;
+
+    @OneToMany(() => User, user => user.products)
+    user = User;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,40 @@
-export class User {}
+import { Product } from "src/products/entities/product.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+export enum Role {
+    User = 'user',
+    Admin = 'admin',
+}
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string = uuid();
+
+    @Column({
+        length: 100,
+    })
+    name: string;
+
+    @Column({ nullable: true })
+    password: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    phone: string;
+
+    @Column({ nullable: true })
+    coutry: string;
+
+    @Column({ nullable: true })
+    city: string;
+
+    @ManyToOne(() => Product, product => product.user)
+    products: Product;
+}
