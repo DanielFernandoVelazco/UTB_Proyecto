4a4ed0fe3aedfdffbd98d31330c2923dca5ed4be
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-11-14 21:39:07 -0500

    Se implemta el servicio Products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,34 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, HttpCode, HttpStatus, Query, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  @HttpCode(HttpStatus.OK)
+  findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 2,
+  ) {
+    return this.productsService.findAll(page, limit);
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+    return this.productsService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.OK)
   remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+    return this.productsService.remove(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) { }
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const newProduct = this.productsRepository.create(createProductDto)
+    return await this.productsRepository.save(newProduct)
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(page: number, limit: number) {
+    return await this.productsRepository.find({
+      take: limit,
+      skip: (page - 1) * limit
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string): Promise<Product> {
+    return await this.productsRepository.findOneBy({ id })
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(
+    id: string,
+    updateProductDto: UpdateProductDto,
+  ): Promise<Product> {
+    console.log('UpdateProductDto', updateProductDto);
+    await this.productsRepository.update(id, updateProductDto)
+    return this.productsRepository.findOneBy({ id })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string): Promise<{ id: string }> {
+    await this.productsRepository.delete(id);
+    return { id }
+  }
+
+  async buyProduct(id: string): Promise<Product> {
+    const product = await this.productsRepository.findOneBy({ id })
+    if (product.stock === 0) {
+      throw new Error('Out of stock')
+    }
+    await this.productsRepository.update(id, {
+      stock: product.stock - 1,
+    })
+    console.log('Product bought successfully');
+    return this.productsRepository.findOneBy({ id })
   }
 }
